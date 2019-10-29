b1c65e7d8f77da22133331509e34b54db22e86d0
rosbel <rlsanroman@gmail.com>
2019-10-29 12:32:47 -0500

    Add styles for Grid component

--- ui/src/Grid/index.jsx
+++ ui/src/Grid/index.jsx
@@ -3,6 +3,7 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
+import './index.scss';
 // Expected Payload:
 /*
  {
