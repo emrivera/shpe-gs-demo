64f14261e0c046c410727d479d97d2dee37c9c93
rosbel <rlsanroman@gmail.com>
2019-10-29 13:46:15 -0500

    Add Basic form to add expenses | WIP: Add people

--- ui/src/Header/index.jsx
+++ ui/src/Header/index.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
+import Button from '@material-ui/core/Button';
+import Form from '../Form';
+
 export default function Header() {
+  const [showForm, setShowForm] = useState(false);
+  const handleAddExpense = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div className="Header">
       <h2>SHPE Demo - Budgeting Tool</h2>
+      <div className="ActionBar">
+        <Button variant="contained" color="primary" onClick={handleAddExpense} disabled={showForm}>
+          Add Expense
+        </Button>
+      </div>
+      <Form
+        show={showForm}
+        onComplete={(payload) => {
+          console.log('Submitted: ', payload);
+          setShowForm(false)
+        }}
+        onCancel={() => {
+          console.log('Canceled');
+          setShowForm(false)
+        }}
+      />
     </div>
   )
 }
