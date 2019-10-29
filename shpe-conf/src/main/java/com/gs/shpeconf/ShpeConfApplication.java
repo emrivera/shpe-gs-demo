090d285fb1e81d6482e667f77bce48ccdf87bc6d
rosbel <rlsanroman@gmail.com>
2019-10-29 11:05:25 -0500

    Add shpe-conf module from start.spring.io

---
+++ shpe-conf/src/main/java/com/gs/shpeconf/ShpeConfApplication.java
@@ -0,0 +1,13 @@
+package com.gs.shpeconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShpeConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShpeConfApplication.class, args);
+	}
+
+}
---
+++ shpe-conf/src/test/java/com/gs/shpeconf/ShpeConfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.shpeconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShpeConfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
