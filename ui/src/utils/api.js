ee7f898c05b6be817c9cf38ffb7ed718f29ac2df
Rosbel Sanroman <rlsanroman@users.noreply.github.com>
2019-10-31 10:37:38 -0700

    Refactor apis/endpoints

--- ui/src/utils/api.js
+++ ui/src/utils/api.js
@@ -1,3 +1,5 @@
+import endpoints from './endpoints';
+
 const headers = {
   'Content-Type': 'application/json'
 };
@@ -21,23 +23,24 @@ async function fetchData(url = '') {
 }
 
 async function fetchPeople() {
-  const { people } = await fetchData('http://localhost:8081/api/people');
+  const { people } = await fetchData(endpoints.people.getPeople);
   return people;
 }
 
-async function fetchExpenses() {
-  const { expenses } = await fetchData('http://localhost:8081/api/expenses');
-  return expenses;
-}
-
 async function postPerson(payload) {
-  const person = await postData('http://localhost:8081/api/person', payload);
+  const person = await postData(endpoints.people.createPerson, payload);
   return person;
 }
 
+async function fetchExpenses() {
+  const { expenses } = await fetchData(endpoints.expenses.getExpenses);
+  return expenses;
+}
+
 async function postExpense(payload) {
-  const expense = await postData('http://localhost:8081/api/expense', payload);
+  const expense = await postData(endpoints.expenses.createExpense, payload);
   return expense;
 }
+
 
 export { postExpense, fetchPeople, fetchExpenses, postPerson };
---
+++ ui/src/utils/endpoints.js
@@ -0,0 +1,12 @@
+const endpoints = {
+  people: {
+    createPerson: 'http://localhost:8081/api/person',
+    getPeople: 'http://localhost:8081/api/people'
+  },
+  expenses: {
+    createExpense: 'http://localhost:8081/api/expense',
+    getExpenses: 'http://localhost:8081/api/expenses'
+  }
+};
+
+export default endpoints;
